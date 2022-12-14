717dc758df5e478da3291006a72f9957df8d7eea
gyuper <gyupermadi@proton.me>
2022-12-14 21:23:50 +0700

    first commit

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -7,4 +7,28 @@ class Auth extends CI_Controller{
     public function register(){
         $this->load->view('auth/register');
     }
+    public function reg_action(){
+        $nama_lengkap = $this->input->post('nama_lengkap');
+        $username = $this->input->post('username');
+        $password = $this->input->post('password');
+        $repassword = $this->input->post('repassword');
+        
+        if ($password != $repassword) {
+            echo "Password Tidak Sama";
+        }
+        else{
+            $data = array(
+                'nama_lengkap' => $nama_lengkap,
+                'username' => $username,
+                'password' => $password,
+            );
+            $result = $this->model->simpan('user, $data');
+            if($result>0){
+                echo "Data User Berhasil Disimpan";
+            }
+            else{
+                echo "Data User Gagal Disimpan";
+            }
+        }
+    }
 }
--- application/views/auth/register.php
+++ application/views/auth/register.php
@@ -33,21 +33,21 @@
   <div class="register-box-body">
     <p class="login-box-msg">Register a new membership</p>
 
-    <form action="../../index.html" method="post">
+    <form action="<?php echo base_url()?>auth/reg_action" method="post">
       <div class="form-group has-feedback">
-        <input type="text" class="form-control" placeholder="Full name">
+        <input type="text" name="nama_lengkap" class="form-control" placeholder="Nama Lengkap">
         <span class="glyphicon glyphicon-user form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="email" class="form-control" placeholder="Email">
+        <input type="email" name="username" class="form-control" placeholder="Username">
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="password" class="form-control" placeholder="Password">
+        <input type="password" name="password" class="form-control" placeholder="Password">
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="password" class="form-control" placeholder="Retype password">
+        <input type="password" name="repassword" class="form-control" placeholder="Retype password">
         <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
       </div>
       <div class="row">
@@ -60,7 +60,7 @@
         </div>
         <!-- /.col -->
         <div class="col-xs-4">
-          <button type="submit" class="btn btn-primary btn-block btn-flat">Register</button>
+          <button type="submit" name="register" class="btn btn-primary btn-block btn-flat">Register</button>
         </div>
         <!-- /.col -->
       </div>
@@ -78,14 +78,6 @@
 <script src="<?php echo base_url()?>assets/bootstrap/js/bootstrap.min.js"></script>
 <!-- iCheck -->
 <script src="<?php echo base_url()?>assets/plugins/iCheck/icheck.min.js"></script>
-<script>
-  $(function () {
-    $('input').iCheck({
-      checkboxClass: 'icheckbox_square-blue',
-      radioClass: 'iradio_square-blue',
-      increaseArea: '20%' // optional
-    });
-  });
-</script>
+
 </body>
 </html>
